52e4bda201d97ab75b766208fa9e6cef006d69c2
Ashish B. Thoppil <ashish.b@polussoftware.com>
2021-11-04 12:38:10 +0530

    Changes from sprint 2
    The commit consists of the second part of the 2nd sprint

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication\AuthController;
 use App\Http\Controllers\EditProfile\EditController;
+use App\Http\Controllers\Course\CourseController;
+use App\Http\Controllers\CourseCategory\CourseCategoryController;
+use App\Http\Controllers\Instructor\InstructorController;
+use App\Http\Controllers\Creator\CreatorController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,5 +35,33 @@ Route::group(['middleware' => 'prevent-back-history'],function() {
 
     Route::get('/edit', [EditController::class, 'edituser'])->name('edituser');
     Route::post('/update',[EditController::class, 'profileUpdate'])->name('profileUpdate');
+
+    Route::get('/manage-courses', [CourseController::class, 'index'])->name('manage-courses');
+    Route::post('/add-course', [CourseController::class, 'saveCourse'])->name('add-course');
+    Route::post('/view-course', [CourseController::class, 'viewCourse'])->name('view-course');
+    Route::post('/edit-course', [CourseController::class, 'editCourse'])->name('edit-course');
+    Route::post('/update-course', [CourseController::class, 'updateCourse'])->name('update-course');
+    Route::post('/delete-course', [CourseController::class, 'deleteCourse'])->name('delete-course');
+
+    Route::get('/manage-course-categories', [CourseCategoryController::class, 'index'])->name('manage-course-categories');
+    Route::post('/add-course-category', [CourseCategoryController::class, 'saveCourseCategory'])->name('add-course-category');
+    Route::post('/view-course-category', [CourseCategoryController::class, 'viewCourseCategory'])->name('view-course-category');
+    Route::post('/edit-course-category', [CourseCategoryController::class, 'editCourseCategory'])->name('edit-course-category');
+    Route::post('/update-course-category', [CourseCategoryController::class, 'updateCourseCategory'])->name('update-course-category');
+    Route::post('/delete-course-category', [CourseCategoryController::class, 'deleteCourseCategory'])->name('delete-course-category');
+
+    Route::get('/manage-instructors', [InstructorController::class, 'index'])->name('manage-instructors');
+    Route::post('/add-instructor', [InstructorController::class, 'saveInstructor'])->name('add-instructor');
+    Route::post('/view-instructor', [InstructorController::class, 'viewInstructor'])->name('view-instructor');
+    Route::post('/edit-instructor', [InstructorController::class, 'editInstructor'])->name('edit-instructor');
+    Route::post('/update-instructor', [InstructorController::class, 'updateInstructor'])->name('update-instructor');
+    Route::post('/delete-instructor', [InstructorController::class, 'deleteInstructor'])->name('delete-instructor');
+
+    Route::get('/manage-creators', [CreatorController::class, 'index'])->name('manage-creators');
+    Route::post('/add-creator', [CreatorController::class, 'saveCreator'])->name('add-creator');
+    Route::post('/view-creator', [CreatorController::class, 'viewCreator'])->name('view-creator');
+    Route::post('/edit-creator', [CreatorController::class, 'editCreator'])->name('edit-creator');
+    Route::post('/update-creator', [CreatorController::class, 'updateCreator'])->name('update-creator');
+    Route::post('/delete-creator', [CreatorController::class, 'deleteCreator'])->name('delete-creator');
 });
 
