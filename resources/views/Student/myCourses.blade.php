359224e267460c6cb299425386f3317bad28e07c
anisht-polus <anish.t@polussoftware.com>
2022-01-10 10:23:00 +0530

    UI improvements
    UI improvements

--- resources/views/Student/myCourses.blade.php
+++ resources/views/Student/myCourses.blade.php
@@ -13,53 +13,7 @@
     cursor: pointer;
 }
   </style>
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow-sm p-3 mb-5 bg-body">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">TY-Mac</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      
-      <form class="mb-2 mb-lg-0 d-flex me-auto mt-2 col-lg-6">
-      @csrf
-        <input id="search-box" class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style=";">
-        <button class="btn btn-outline-success" type="submit" id="search-btn">Search</button>
-      </form>
-
-      <ul class="navbar-nav me-2">
-      @if (Auth::check())
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('edituser') }}"><img src="{{ asset('/storage/images/'.Auth::user()->image) }}" class="img-fluid rounded-circle float-start me-2 mt-1" alt="" style="width:20px; height:20px;"> {{Auth::user()->firstname}}</a>
-        </li>
-        @endif
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('student.courses.get') }}">All Courses</a>
-        </li>
-        
-        @if (Auth::check())
-        <li class="nav-item border-bottom border-warning border-3">
-          <a class="nav-link active" href="{{ route('my-courses') }}">My courses</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('logout') }}">Logout</a>
-        </li>
-        @else
-        <li class="nav-item">
-        <a class="nav-link" href="#signup" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#login" data-bs-toggle="modal" data-bs-target="#loginModal">Login</a></li>
-        </li>
-        @endif
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+@extends('header')
 <!-- login modal -->
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -287,9 +241,10 @@
 
 
                                             <div class="col-lg-4 col-md-4 col-sm-4 col-12  mb-4">
-                                                <div class="card-1">
+                                                <div class="card-1 position-relative">
                                                     <img src="courselist/Illustration/Mask Group 2.jpg"
                                                         class="card-img-top" alt="...">
+                                                        <span class="badge text-danger border border-1 border-danger position-absolute start-0 top-0 ms-3 mt-3">Live</span>
                                                     <div class="card-body">
                                                         <h5 class="card-title text-center">Fundamentals of Google Docs &
                                                             Google Drive 2</h5>
