248ece04b0d1a01b7467531b59eec937089d6b47
Anjali-polus <92292006+Anjali-polus@users.noreply.github.com>
2022-01-13 04:17:31 -0800

    changes

--- resources/views/Layouts/enrollCourse.blade.php
+++ resources/views/Layouts/enrollCourse.blade.php
@@ -12,6 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/assets/enrollcourse.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/styles.css') }}">
     
     
 </head>
