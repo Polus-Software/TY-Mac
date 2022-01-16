7ccf42c5af9f4fd745c13c8a25052067a0272fdf
Anjali-polus <92292006+Anjali-polus@users.noreply.github.com>
2022-01-16 02:27:37 -0800

    changes

--- resources/views/Student/myCourses.blade.php
+++ resources/views/Student/myCourses.blade.php
@@ -37,66 +37,39 @@
             </div>
         </div>
     </div>
-</div>
-<!-- signup modal ends -->
-    <section class="pt-5">
-        <div class="container">
-            <div class="row border-bottom mt-5 pb-3">
-                <div class="col-lg-6 col-md-6 col-sm-7 col-12">
-                    <h3 class="think-tab-title">Current Live Classes</h3>
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-5 col-12 d-flex  justify-content-lg-end justify-content-md-end justify-content-sm-end">
-                    <ul class="nav nav-tabs border border-dark">
-                        <li class="nav-item active">
-                            <button class="nav-link active" id="live-tab" type="button" data-bs-toggle="tab"
-                                data-bs-target="#live" role="tab" aria-controls="live" aria-selected="true"
-                                data-bs-toggle="tab">Live</button>
-                        </li>
-                        <li class="nav-item">
-                            <button class="nav-link" id="upcoming-tab" type="button" data-bs-toggle="tab"
-                                data-bs-target="#upcoming" role="tab" aria-controls="upcoming" aria-selected="false"
-                                data-bs-toggle="tab">Upcoming</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
+</section>
 
 
 
 
-    <section class="mt-4">
-        <div class="container">
-            <div class="row">
-                <div class="tab-content">
-                    <div id="live" class="tab-pane fade show active" aria-labelledby="live-tab">
-                        <div class="col-lg-12">
-                            @if(!empty($liveSessionDetails))
-                            <div id="liveCarousel" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <div class="row">
-                                            
-                                            @foreach($liveSessionDetails as $liveSessionDetail)
-                                            <div class="col-lg-4 col-md-4 col-sm-4 col-12 mb-4">
-                                                <div class="card-1">
-                                                    <img src="{{ asset('/storage/courseTumbnailImages/' . (($liveSessionDetail['course_thumbnail_image']) ? $liveSessionDetail['course_thumbnail_image'] : 'defaultImage.png')) }}"
-                                                        class="card-img-top" alt="...">
-                                                        <span class="badge text-danger border border-1 border-danger position-absolute start-0 top-0 ms-3 mt-3">Live</span>
-                                                    <div class="card-body">
-                                                        <h5 class="card-title text-center">{{ $liveSessionDetail['session_title'] }}</h5>
-                                                        <p class="card-text text-sm-start text-truncate">{{ $liveSessionDetail['course_desc'] }}</p>
-                                                        <ul class="list-group list-group-flush">
-                                                            <li class="list-group-item">
-                                                                <div class="row">
-                                                                    <div class="col-lg-6 col-sm-6 col-6">
-                                                                        <p><i class="far fa-user pe-1"></i> {{ $liveSessionDetail['instructor'] }}</p>
-                                                                    </div>
-                                                                    <div class="col-lg-6 col-sm-6 col-6">
-                                                                        <p class="text-end"><i
-                                                                                class="far fa-user pe-1"></i> {{ $liveSessionDetail['course_diff'] }}</p>
-                                                                    </div>
+<section class="mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="tab-content">
+                <div id="live" class="tab-pane fade show active" aria-labelledby="live-tab">
+                    <div class="col-lg-12">
+                        @if(!empty($liveSessionDetails))
+                        <div id="liveCarousel" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="row">
+
+                                        @foreach($liveSessionDetails as $liveSessionDetail)
+                                        <div class="col-lg-4 col-md-4 col-sm-4 col-12 mb-4">
+                                            <div class="card-1">
+                                                <img src="courselist/Illustration/Mask Group 2.jpg" class="card-img-top" alt="...">
+                                                <span class="badge text-danger border border-1 border-danger position-absolute start-0 top-0 ms-3 mt-3">Live</span>
+                                                <div class="card-body">
+                                                    <h5 class="card-title text-center">{{ $liveSessionDetail['session_title'] }}</h5>
+                                                    <p class="card-text text-sm-start text-truncate">{{ $liveSessionDetail['course_desc'] }}</p>
+                                                    <ul class="list-group list-group-flush">
+                                                        <li class="list-group-item">
+                                                            <div class="row">
+                                                                <div class="col-lg-6 col-sm-6 col-6">
+                                                                    <p><i class="far fa-user pe-1"></i> {{ $liveSessionDetail['instructor'] }}</p>
+                                                                </div>
+                                                                <div class="col-lg-6 col-sm-6 col-6">
+                                                                    <p class="text-end"><i class="far fa-user pe-1"></i> {{ $liveSessionDetail['course_diff'] }}</p>
                                                                 </div>
                                                             </div>
                                                         </li>
@@ -131,32 +104,31 @@
                         </div>
                         @endif
                     </div>
-                    <div class="tab-content">
-                    <div id="upcoming" class="tab-pane fade" aria-labelledby="upcoming-tab">
-                        <div class="col-lg-12">
-                            @if(!empty($upComingSessionDetails))
-                            <div id="upcomingCarousel" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <div class="row">
-                                            @foreach($upComingSessionDetails as $upComingSessionDetail)
-                                            <div class="col-lg-4 col-md-4 col-sm-4 col-4 mb-4">
-                                                <div class="card-1">
-                                                    <img src="{{ asset('/storage/courseTumbnailImages/' . (($upComingSessionDetail['course_thumbnail_image']) ? $upComingSessionDetail['course_thumbnail_image'] : 'defaultImage.png')) }}"
-                                                        class="card-img-top" alt="...">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title text-center">{{ $upComingSessionDetail['session_title'] }}</h5>
-                                                        <p class="card-text text-sm-start text-truncate">{{ $upComingSessionDetail['course_desc'] }}</p>
-                                                        <ul class="list-group list-group-flush">
-                                                            <li class="list-group-item">
-                                                                <div class="row">
-                                                                    <div class="col-lg-6 col-sm-6 col-6">
-                                                                        <p><i class="far fa-user pe-1"></i>{{ $upComingSessionDetail['instructor'] }}</p>
-                                                                    </div>
-                                                                    <div class="col-lg-6 col-sm-6 col-6">
-                                                                        <p class="text-end"><i
-                                                                                class="far fa-user pe-1"></i> {{ $upComingSessionDetail['course_diff'] }}</p>
-                                                                    </div>
+                </div>
+            </div>
+            <div class="tab-content">
+                <div id="upcoming" class="tab-pane fade" aria-labelledby="upcoming-tab">
+                    <div class="col-lg-12">
+                        @if(!empty($upComingSessionDetails))
+                        <div id="upcomingCarousel" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <div class="row">
+                                        @foreach($upComingSessionDetails as $upComingSessionDetail)
+                                        <div class="col-lg-4 col-md-4 col-sm-4 col-4 mb-4">
+                                            <div class="card-1">
+                                                <img src="courselist/Illustration/Mask Group 2.jpg" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title text-center">{{ $upComingSessionDetail['session_title'] }}</h5>
+                                                    <p class="card-text text-sm-start text-truncate">{{ $upComingSessionDetail['course_desc'] }}</p>
+                                                    <ul class="list-group list-group-flush">
+                                                        <li class="list-group-item">
+                                                            <div class="row">
+                                                                <div class="col-lg-6 col-sm-6 col-6">
+                                                                    <p><i class="far fa-user pe-1"></i>{{ $upComingSessionDetail['instructor'] }}</p>
+                                                                </div>
+                                                                <div class="col-lg-6 col-sm-6 col-6">
+                                                                    <p class="text-end"><i class="far fa-user pe-1"></i> {{ $upComingSessionDetail['course_diff'] }}</p>
                                                                 </div>
                                                             </div>
                                                         </li>
@@ -226,10 +198,8 @@
                                 <h5 class="card-title pb-3">
                                     {{ $singleEnrolledCourse['course_title'] }}
                                 </h5>
-                                <p class="card-text position-relative">
-                                    <span class="think-truncated-text">
-                                        {{Str::limit($singleEnrolledCourse['description'], 180, ' (...)')}}
-                                    </span>
+                                <p class="card-text">
+                                    {{ $singleEnrolledCourse['description'] }}
                                 </p>
                                 <div class="row">
                                     <div class="col-lg-5 col-md-12 col-sm-12 col-12 mb-3">
