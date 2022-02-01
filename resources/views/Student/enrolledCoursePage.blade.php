8e4cd66908a412c3ea91895efe0fd0a5fb62c9fe
Anjali-polus <92292006+Anjali-polus@users.noreply.github.com>
2022-01-31 22:51:32 -0800

    changes

--- resources/views/Student/enrolledCoursePage.blade.php
+++ resources/views/Student/enrolledCoursePage.blade.php
@@ -58,37 +58,7 @@
     border-bottom:1px solid #F5BC29;
     padding-bottom:30px;
 }
-/* .modal-dialog {
-    max-width: 895px !important;
-} */
-.modal-body {
-    height: 31rem;
-}
-.modal-header {
-    padding: 1.5rem 1rem 1rem 4rem;
-    height: 4rem;
-    border-bottom: none;
-}
-/* Chart modal */
   </style>
-  <div class="modal fade" id="chartModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div id="graph_div">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 <header class="d-flex align-items-center mb-3 mt-4">
     <div class="container">
         <div class="row mt-5">
@@ -317,24 +287,12 @@
                     <div class="tab-pane fade show {{($userType == 'student') ? 'active' : ''}}" id="v-pills-cohortSchedule" role="tabpanel" aria-labelledby="v-pills-cohortSchedule">
                         <div class="card card-2 mb-3">
                             <div class="card-body">
-                                <div class="row">
-                                    <div class="col-lg-9 col-md-9 col-sm-9 col-12">
-                                        <h5 class="card-title pt-2">Session Info</h5>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-sm-3 col-12">
-                                    @if($userType == 'student')
-                                        <a class="btn btn-secondary think-btn-secondary" href="{{ route('study.materials') }}?course={{$course['id']}}">Go to study materials</a>
-                                    @endif
-                                    </div>
-                                </div>
-                                <hr>
-                                @php ($slno = 0)
+                                <h5 class="card-title border-bottom pt-2 pb-2">Session info</h5>
                                 @foreach($topicDetails as $topicDetail)
                                 
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-12">
-                                        @php ($slno = $slno + 1)
-                                        <h6 class="card-title pt-2" data-id="{{ $topicDetail['topic_id'] }}">Session {{$slno}} - {{ $topicDetail['topic_title'] }}</h6>
+                                        <h6 class="card-title pt-2" data-id="{{ $topicDetail['topic_id'] }}">{{ $topicDetail['topic_title'] }}</h6>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-12 d-flex justify-content-lg-end justify-content-md-end mt-2">
                                         @if($topicDetail['liveId'] == null)
@@ -346,7 +304,7 @@
                                         @elseif($topicDetail['liveId'] == "Over")
                                         <a style="background-color: #f0f0f0;color: black;" type="button" class="btn" href=""><i class="fas fa-undo pe-2"></i>View again</a>
                                         @else
-                                        <a style="background-color: #74648C;color: white;" type="button" target="_blank" class="btn" href="/session-view/{{ $topicDetail['liveId'] }}?batchId={{ isset($selectedBatch) ? $selectedBatch : '' }}"><i class="fas fa-eye pe-2"></i>View live session</a>
+                                        <a style="background-color: #74648C;color: white;" type="button" class="btn" href="/session-view/{{ $topicDetail['liveId'] }}"><i class="fas fa-eye pe-2"></i>View live session</a>
                                         @endif
                                     </div>
                                 </div>
@@ -390,7 +348,7 @@
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-lg-12 mt-3">
+                                            <div class="col-lg-12">
                                                 <h6 class="card-title">{{ $recommendation['topic_title'] }}</h6>
                                                 <ul class="list-group list-group-flush border-bottom pb-3">
                                                     <li class="ms-3 border-0 pb-2">{{ $recommendation['content_title'] }}</li>
@@ -448,7 +406,7 @@
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-lg-12 mt-3">
+                                            <div class="col-lg-12">
                                                 <h6 class="card-title">{{ $recommendation['topic_title'] }}</h6>
                                                 <ul class="list-group list-group-flush border-bottom pb-3">
                                                     <li class="ms-3 border-0 pb-2">{{ $recommendation['content_title'] }}</li>
@@ -473,7 +431,7 @@
                                     <h2 class="accordion-header" id="headingOne">
                                         <button class="accordion-button shadow-none text-capitalize mb-2p-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne_{{ $student->id }}" aria-expanded="true" aria-controls="collapseOne_{{ $student->id }}">
                                         <img src="{{ asset('/storage/images/user.png') }}"  class="rounded-circle me-3" alt="" style="width:40px; height:40px;"><p class="pt-3 card-title-4">{{ $student->firstname .' '. $student->lastname }}</p>
-                                        <a href="#" class="btn btn-outline-secondary text-dark ms-auto"><i class="fas fa-comments pe-2"></i>Message</a>
+                                        <a href="#" class="btn btn-outline-secondary text-dark ms-auto"><i class="fas fa-comments pe-2"></i>Messsge</a>
                                     </button>
                                        
                                     </h2>
@@ -491,8 +449,7 @@
                                                                     <a href="#" class="btn btn-primary w-100">1-on-1 Session</a>
                                                                 </div>
                                                                 <div class="col-lg-6">
-                                                                    @csrf
-                                                                    <button type="button" class="btn btn-outline-secondary text-dark w-100" data-bs-toggle="modal" data-bs-target="#chartModal" data-bs-student-id="{{$recommendation['student_id']}}"  data-bs-topic-id="{{$recommendation['topic_id']}}">Chart</button>
+                                                                    <a href="#" class="btn btn-outline-secondary text-dark w-100">Chart</a>
                                                                 </div> 
                                                             </div>
                                                             <div class="row mt-3">
@@ -735,25 +692,14 @@
                                                                  Assignment: {{$assignment['assignment_title']}}</strong>
                                                                 </div>
                                                             <div class="card-body">
-                                                            
-                                                            <div class="llpcard-inner bg-light mt-3 mb-3 p-3">
-                                                            <h5 class="card-title">Type your comment here</h5>
-                                                            <form action="{{ route('submit.assignment') }}" enctype="multipart/form-data" method="POST" class="row g-3 llp-form">
-                                                            @csrf
-                                                            <input type="hidden" name="assignment_id" value="{{ $assignment['id'] }}" />
-                                                            <textarea style="height: 110px;" class="form-control" type="text" name="assignment_comment" placeholder="Type your comment here.."></textarea>
-                                                                    <div class="card card-body mb-3" style="background-color: transparent;background-clip: border-box;border: none;"> 
-                                                                        <div class="row p-2 flex-fill bd-highlight">
-                                                                            <div class="col-lg-3">Attach File:</div>
-                                                                                <div class="col-lg-5 col-12"><label>Upload from device</label>
-                                                                                    <input class="form-control" type="file" name="assignment_upload">
-                                                                                    <small class="fst-italic">Supported File Formats are:  ppt, pdf, doc, docx,</small>
-                                                                                </div>
-                                                                            <!-- <div class="col-lg-3 pt-4"><a class="btn btn-sm btn-outline-secondary" style="height: 37px;line-height: 27px;">Add external link</a></div> -->
-                                                                        </div>
-                                                                        <div class="col-12 text-end mt-4">
-                                                                            <a class="btn btn-sm btn-outline-secondary me-3">Cancel</a>
-                                                                            <button type="submit" style="font-size: 14px;font-weight: 100;color: #ffffff;" class="btn btn-sm btn-dark">Submit</a>
+                                                                <p class="card-text">{{$assignment['assignment_description']}}</p>
+                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                                                        <div class="row">
+                                                                            <div class="col-lg-10">
+                                                                                <p style="color:#6E7687;" class="mt-4">External Link</p>
+                                                                                <a target="_blank" href="/storage/assignmentAttachments/{{$assignment['document']}}">{{$assignment['document']}}</a></p>
+                                                                            </div>
                                                                         </div>
                                                                     </div>
 
@@ -802,7 +748,7 @@
                                                         <div class="col-4 m-auto text-center">
                                                         <a card-id="{{ $topicDetail['topic_id'] }}" class="btn btn-sm btn-dark me-3 start_assignment" href="">Start Assignment</a>
                                                         </div>
-                                                   
+                                                  
 
                                                     @else
                                                         <div class="col-12 m-auto text-center">
@@ -1112,86 +1058,6 @@ document.getElementById('submitStudentQuestion').addEventListener('click', funct
 });
 </script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-@if($userType == 'instructor')
-<script>
-    
-    
-
-            function drawChart(count, contents, student) {
-                console.log(contents[0]['likes']);
-                var gdata = new google.visualization.DataTable();
-
-                gdata.addColumn('string', 'Subtopics');
-                gdata.addColumn('number', 'Feedbacks');
-                for(i=0;i<count;i++){
-                    gdata.addRows([
-                        [contents[i]['content_title'],contents[i]['likes']]
-                    ]);
-                }
-                
-
-                var options = {
-                chart: {
-                    title: student + "'s Activities"
-                },
-                width: 900,
-                height: 470,
-                colors: ['#A26B05'],
-                vAxis: {
-                    format: '0'
-                    },
-                };
-
-                var chart = new google.charts.Line(document.getElementById('graph_div'));
-                chart.draw(gdata, google.charts.Line.convertOptions(options));
-            }
-      
-    let contentCount = 0;
-    let contentArr = [];
-
-    document.getElementById('chartModal').addEventListener('show.bs.modal', function (event) {
-        var button = event.relatedTarget
-        var student = button.getAttribute('data-bs-student-id');
-        var topic = button.getAttribute('data-bs-topic-id');
-        var course = document.getElementById('course_id');
-        
-        let path = "{{ route('get-individual-student-chart') }}?student=" + student +"&topic=" + topic +"&course=" + course;
-        fetch(path, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "X-CSRF-Token": document.querySelector('input[name=_token]').value
-          },
-        }).then((response) => response.json()).then((jsondata) => {
-            google.charts.load('current', {'packages':['line']});
-            
-            google.charts.setOnLoadCallback(function(){ drawChart(jsondata.contentCount, jsondata.contents, jsondata.student) });
-        });
-
-        setTimeout(hideAxisLabels, 500);
-        function hideAxisLabels(){
-            let axisElements = document.getElementById('graph_div').getElementsByTagName('g')[4].getElementsByTagName('text');   
-            let axisElementsLength = axisElements.length;
-            axisElements[parseInt(axisElementsLength) - 2].style.display = 'none';
-            axisElements[parseInt(axisElementsLength) - 4].style.display = 'none';
-
-            let title = document.getElementById('graph_div').getElementsByTagName('g')[0].getElementsByTagName('text')[0];
-            title.setAttribute('fill', '#2C3443');
-            title.style.fontSize = "16px";
-            title.style.fontWeight = "600";
-
-            let lineElement = document.getElementById('graph_div').getElementsByTagName('g')[2]; 
-            let path = lineElement.getElementsByTagName('path')[0];
-            let circle = lineElement.getElementsByTagName('circle');
-            path.setAttribute('stroke-width', '4');
-            for(i=0;i<circle.length;i++){
-                circle[i].setAttribute('fill-opacity', 1);
-            }
-        }
-    });
-</script>
-@elseif($userType == 'student')
 <script>
     google.charts.load('current', {
         'packages': ['bar']
@@ -1227,5 +1093,4 @@ document.getElementById('submitStudentQuestion').addEventListener('click', funct
         chart.draw(data, google.charts.Bar.convertOptions(options));
     }
 </script>
-@endif
 @endpush
